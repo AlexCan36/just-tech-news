f99abd48c61eeeb788e3c1b0572c7f18d6e39825
Alex Canez <el.winy.36@gmail.com>
2022-06-02 17:04:33 -0600

    finished

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-app.use(routes); 
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
